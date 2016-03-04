6ed081fe511a6a4a8b5b3738ff3dfcaaaf673c22
Alan Darmasaputra <alan.darmasaputra@ti.ukdw.ac.id>
2016-03-04 10:33:19 +0700

    laravel new

---
+++ src/biro3ukdw/resources/views/layout/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('head_title')</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+		<link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+		<link href="{{ asset('exambro/css/exambro.css')}}" rel="stylesheet" type="text/css">
+		<script src="{{ asset('jquery/jquery.js')}}" type="text/javascript"></script>
+		<script src="{{ asset('bootstrap/js/bootstrap.min.js')}}" type="text/javascript"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		@yield('head_addition')
+    </head>
+    <body>
+		<nav class="navbar navbar-default" >
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+				  	</button>
+				  <a class="navbar-brand" href="{{ url('/') }}">Title</a>
+				</div>
+    			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a href="#">Item1</a></li>
+						<li><a href="#">Item2</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						@if(Auth::check())
+						<li><a href="#"><strong>{{Auth::user()->name}}</strong></a></li>
+						<li><a href="{{url('/logout')}}">Logout</a></li>
+						@endif
+					</ul>
+				</div>
+			</div>
+		</nav>
+		@include('errors.common')
+		@yield('body_content')
+    </body>
+</html>
---
+++ src/biro3ukdw/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.app')
+@section('body_content')
+<div class="container-fluid">
+    <div class="text-center">
+        <div class="page-header"><h1>Page Header</h1></div>
+        <h3>Subheader</h3>
+        <div class="container"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse viverra consectetur imperdiet. Aenean dignissim leo ac mauris consequat, in elementum eros aliquam. Aliquam vehicula metus eu ante imperdiet, in bibendum sem ornare. Donec nisi tortor, iaculis nec imperdiet id, gravida et nibh. Aenean feugiat neque mi, tristique vestibulum metus ullamcorper vel. Nullam condimentum justo ac fermentum ullamcorper. Aenean rutrum pharetra erat, a cursus mauris semper id. Quisque dapibus imperdiet iaculis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec at volutpat ligula. Nullam at ipsum ullamcorper, imperdiet neque ac, commodo metus.
+Vestibulum vestibulum varius consequat. Nunc in eleifend sem. Aliquam erat volutpat. Mauris sollicitudin nulla a augue scelerisque, a varius nisl porttitor. Aliquam nec imperdiet libero. Maecenas massa quam, ullamcorper et iaculis vehicula, facilisis quis felis. Nam eget ultrices est, eu tempor elit. Curabitur nisi nunc, luctus ut consequat eget, aliquet at neque. Morbi eu dui at quam egestas condimentum. Curabitur ut urna sed urna mattis ultrices. Mauris sit amet efficitur augue. Sed posuere finibus eros. Nullam semper eros eget mauris elementum, non vehicula sem dignissim.
+Integer sollicitudin purus libero, at ornare est congue sit amet. Donec sed nibh quam. Duis gravida sagittis erat, eu finibus orci aliquet non. Sed imperdiet purus ut arcu molestie, sed hendrerit augue fermentum. Fusce at vestibulum risus. Donec non bibendum lacus. Nulla semper massa nec purus scelerisque tincidunt. Nulla facilisi.
+Aenean efficitur, ante vitae dignissim porta, arcu dui iaculis augue, a rhoncus dui odio sit amet elit. Donec suscipit vestibulum quam in gravida. Quisque fermentum mi ac dui ullamcorper, dapibus blandit orci tempor. Donec quis elit aliquam enim elementum aliquet. Suspendisse potenti. Nullam placerat viverra tincidunt. Curabitur in ligula venenatis, ultrices massa consectetur, faucibus urna. Cras vel felis eu est mollis tincidunt a vitae magna.
+Aenean at viverra dolor, nec tincidunt ipsum. Nulla facilisi. Aenean vitae ultricies felis. Mauris facilisis fringilla augue, ut ultricies nunc eleifend eget. Vestibulum tempor, dolor at posuere interdum, nisl elit fringilla purus, ac blandit lectus risus et nisl. Pellentesque nunc ante, malesuada in lectus eu, laoreet suscipit lectus. Mauris eget velit vel ex dapibus varius a non ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed sit amet ultrices massa.
+            </p>
+        </div>
+    </div>
+</div>
+@endsection
