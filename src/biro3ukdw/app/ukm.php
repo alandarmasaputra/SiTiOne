e9fb37ee472abf2c122d389002d9587c470693b5
Alan Darmasaputra <alan.darmasaputra@ti.ukdw.ac.id>
2016-03-09 11:54:13 +0700

    Ganti nama jadi huruf besar Admin Controller ditambahi guard

--- src/biro3ukdw/app/ukm.php
+++ src/biro3ukdw/app/ukm.php
@@ -4,9 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ukm extends Model
+class Ukm extends Model
 {
     public function content(){
-    	return $this->hasMany("App\ukm_content");
+    	return $this->hasMany("App\UkmContent");
     }
 }
