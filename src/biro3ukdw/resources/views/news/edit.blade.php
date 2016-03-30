7184a328bde6823ceb342907fc8e35f33ef34d2a
Welly Purnama <welly.purnama@ti.ukdw.ac.id>
2016-03-30 19:11:49 +0700

    news tampilan baru

--- src/biro3ukdw/resources/views/news/edit.blade.php
+++ src/biro3ukdw/resources/views/news/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 @section('head_title')
-Edit NEWS - Biro3 | UKDW
+Edit News - Biro3 | UKDW
 @endsection
 
 @section('head_addition')
@@ -31,26 +31,31 @@ use App\AppUtility;
         </h2>
     </div>
     <!-- jangan diubah ubah -->
-    <div class="text-center editor">
+    <div class="text-left editor">
         <form method="post" action="{{ url('/news/update/'.$news->id) }}" enctype="multipart/form-data"><!--  -->
             {!! csrf_field() !!}
             <div class="editor-header">
-                <div><label>Photo</label></div>
-                
+                     
                 @if($news->header_pic)
-                <img id="header-pic-show" src="{{ AppUtility::get_image_data($news->header_pic) }}">
+                <div id="header-pic-show" style="background-image: url('{{AppUtility::get_image_data($news->header_pic) }}'); height: 400px">
+                    <div id="header-pic-alert" style="display:none;">please select valid file type. The supported file types are .jpg, .png, .bmp</div>
+                    <label>Upload foto:</label> <input name="header-pic" id="header-pic" type="file" onchange="imageupload(this)">
+                </div>
                 @else
-                <img id="header-pic-show">
+                <div id="header-pic-show">
+                    <div id="header-pic-alert" style="display:none;">please select valid file type. The supported file types are .jpg, .png, .bmp</div>
+                    <label>Upload foto:</label> <input name="header-pic" id="header-pic" type="file" onchange="imageupload(this)">
+                </div>
                 @endif
-                <br>
                 <textarea name="header-pic-old" style="display:none;">{{ $news->header_pic }}</textarea>
-                <div id="header-pic-alert" style="display:none;">please select valid file type. The supported file types are .jpg, .png, .bmp</div>
-                <input name="header-pic" id="header-pic" type="file" onchange="imageupload(event,this)">
                 <h2>
-                <div><label>Nama News</label></div>
-                <input name="title" type="text" value="{{ $news->name }}" required></h2>
+                    <label>Nama News:</label>
+                    <input name="title" id="editor-header-title" type="text" value="{{ $news->name }}" required>
+                </h2>
             </div>
-            <hr>
+            <h2 class="editor-content-label text-left">
+                Content
+            </h2>
             <div id="editor-content">
                 <?php
                     //start counter
@@ -70,8 +75,9 @@ use App\AppUtility;
                     @elseif($content->type == 'i')
                         <input class="editor-item-id" type="hidden" name="type-{{$i}}" value="image" id="type-{{$i}}">
                         <textarea class="content-old" id="content-{{$i}}-old" name="content-{{$i}}-old" style="display:none;">{{ $content->content }}</textarea>
-                        <img class="editor-image-show" id="img-{{$i}}-show" src="{{AppUtility::get_image_data($content->content)}}">
-                        <input class="editor-image" type="file" onchange="imageupload(this)" id="img-{{$i}}" name="img-{{$i}}">
+                        <div class='editor-image-show' style="background-image:url('{{ AppUtility::get_image_data($content->content) }}'); height:400px;'">
+                            <input class='editor-image' type='file' onchange='imageupload(this)'>
+                        </div>
                     @endif
                     </div>
                     {!! newButtonPanel() !!}
@@ -85,12 +91,14 @@ use App\AppUtility;
                 ?>
                 @endforeach
             </div>
-            <button id="new-paragraph"><span class="glyphicon glyphicon-plus"></span> Paragraph</button>
-            <button id="new-image"><span class="glyphicon glyphicon-plus"></span> Image</button>
+            <button type="button" id="new-paragraph"><span class="glyphicon glyphicon-plus"></span> Paragraph</button>
+            <button type="button" id="new-image"><span class="glyphicon glyphicon-plus"></span> Image</button>
             
             <br>
             <br>
-            <input type="submit">
+            <div class="text-center">
+                <input type="submit">
+            </div>
             
             <!-- include harus sebelum tutup form -->
             <script src="{{ url('utility/editor/editor.js') }}"></script>
@@ -105,9 +113,9 @@ use App\AppUtility;
 
 function newButtonPanel(){
     return "<div class='editor-button'>"
-                ."<button onclick='insertParagraphBefore(event,this)'><span class='glyphicon glyphicon-plus'></span>Text</button>"
-                ."<button onclick='insertImageBefore(event,this)'><span class='glyphicon glyphicon-plus'></span>Image</button>"
-                ."<button onclick='deleteParent(event,this)'><span class='glyphicon glyphicon-remove'></span>Delete</button>"
+                ."<button type='button' onclick='insertParagraphBefore(event,this)'><span class='glyphicon glyphicon-plus'></span>Text</button>"
+                ."<button type='button' onclick='insertImageBefore(event,this)'><span class='glyphicon glyphicon-plus'></span>Image</button>"
+                ."<button type='button' onclick='deleteParent(event,this)'><span class='glyphicon glyphicon-remove'></span>Delete</button>"
             ."</div>";
 }
 
