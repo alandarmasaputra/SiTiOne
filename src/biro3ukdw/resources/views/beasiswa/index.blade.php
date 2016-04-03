8ee016a44622a0c11c9a43242fe38b7e3867f4d4
Christiana Wahyunita <christianawahyunita@gmail.com>
2016-04-03 09:51:10 +0700

    harus pakai _

--- src/biro3ukdw/resources/views/beasiswa/index.blade.php
+++ src/biro3ukdw/resources/views/beasiswa/index.blade.php
@@ -45,12 +45,12 @@ use Carbon\Carbon;
 								 if(strpos($beasiswa->kategori,'internal')!==false){
 									 $internal = true;
 							 ?>
-							 	style="background-image: url('{{url('style/images/ico/beasiswa dalam negri 2.png')}}')"
+							 	style="background-image: url('{{url('style/images/ico/beasiswa_dalam.png')}}')"
 							 <?php
 								 }else{
 									 $internal = false;
 							 ?>
-							 	style="background-image: url('{{url('style/images/ico/beasiswa luar negri fix.png')}}')"
+							 	style="background-image: url('{{url('style/images/ico/beasiswa_luar.png')}}')"
 							 <?php
 							 }}
 							 ?>>
