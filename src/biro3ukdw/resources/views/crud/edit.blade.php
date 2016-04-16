25692df2ce2c381d9323068294f1b06a0b813b29
Jonathan Aditya <jonathan.aditya@ti.ukdw.ac.id>
2016-04-16 22:19:30 +0700

    edit user

--- src/biro3ukdw/resources/views/crud/edit.blade.php
+++ src/biro3ukdw/resources/views/crud/edit.blade.php
@@ -8,11 +8,14 @@ active
 
 @extends('layout.app')
 @section('body_content')
-
+<?php 
+use App\AppUtility;
+$addition = $user->addition() 
+?>
 <div class="card">
  <h1>Data User</h1> <hr>
 
-
+<!--
  {!! Form::model($user, [
     'method' => 'PATCH',
     'route' => ['user.update', $user->id]
@@ -40,7 +43,47 @@ active
 {!! Form::submit('Update', ['class' => 'btn btn-primary']) !!}
 
 {!! Form::close() !!}
- 
+ -->
+ <div class="row">
+                <div class="col-md-12 body-content card">
+                    <!-- jangan diubah ubah -->
+                    <div>
+                        <form method="post" action="{{ url('/edituser/edit/'.$user->id) }}" enctype="multipart/form-data"><!--  -->
+                            {!! csrf_field() !!}
+                            
+                                <div class="editor-header-input-group">
+                                    <div class="editor-header-input-control">
+                                        <label>Jabatan User:</label>
+                                        <input type="text" name="jabatan" <?php if($addition){ echo 'value=$addition->jabatan'; } ?>>
+                                    </div>
+                                    <div class="editor-header-input-control">
+                                        <label>Email User:</label>
+                                        <input type="text" name="email" value="{{ $user->email }}"  >
+                                    </div>
+                                    <div class="editor-header-input-control">
+                                        <label>Telepon User:</label>
+                                        <input type="text" name="email" <?php if($addition){ echo 'value="$addition->phone"'; } ?>  >
+                                    </div>
+                                       
+                                </div>
+                            </div>
+                            
+                            <br>
+                            <br>
+                            <div class="text-center">
+                                <input type="submit">
+                            </div>
+
+                            <!-- include harus sebelum tutup form -->
+                            <script src="{{ url('utility/editor/editor.js') }}"></script>
+                            <script src="{{ url('utility/editor/editor_load.js') }}"></script>
+                            <script src="{{ url('utility/editor/tagmanager.js')}}"></script>
+                            <script src="{{ url('utility/editor/tagmanager_load.js')}}"></script>
+                        </form>
+                    </div>
+                    <!-- -->
+                </div>
+            </div>
 
 		@stop
 			
