d49f1a47b5e162bfc9ee41ca4f23d5242b229aae
Welly Purnama <welly.purnama@ti.ukdw.ac.id>
2016-04-27 01:06:45 +0700

    design crud user

--- src/biro3ukdw/resources/views/crud/index.blade.php
+++ src/biro3ukdw/resources/views/crud/index.blade.php
@@ -8,65 +8,289 @@ active
 
 @extends('layout.app')
 @section('body_content')
-<div class="container card">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<h1>Data User</h1>
-			<hr>
-			
-			<a href="{{ url('/cruduser/new') }}"><b>Create New User<b></a>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<table class="table table-bordered">
-				<thead>
-					<th>Username</th>
-					<th>Email</th>
-					<th>Level</th>
-					<th>Status</th>
-					<th></th>
-					<th></th>
-					<th></th>
-				</thead>
-				<tbody>
-					@foreach ($user as $data)
-					<tr>
-						<td>{{ $data->username }}</td>
-						<td>{{ $data->email}}</td>
-						@if($data->auth_level=="0")
-						<td>Super Admin</td>
-						@elseif($data->auth_level=="1")
-						<td>Admin</td>
-						@elseif($data->auth_level=="2")
-						<td>Staff</td>
-						@else
-						<td>Volunteer</td>
-						@endif
+<section class="container">
+       
+            <div class="rows" >
+            
+                    <div class="blog-item">
+                    	<div class="text-center">
+                        <h1>CRUD USER </h1>
+                        <hr>
+                        </div>
+                        <div class="row">  
+                            <div class="col-xs-12 col-sm-12 blog-content">
+                                        <div class="col-md-12 blog-content">
+                                    <!-- Nav tabs --><div class="card">
+                                  
 
-						@if($data->is_aktif=="0")	
-						<td>Tidak aktif</td>
-						@else
-						<td>Aktif</td>
-						@endif	
-						<td>                
-						{!! Form::open(['method' => 'DELETE', 'route' => ['user.destroy', $data->id]]) !!}
-						{!! Form::submit('Hapus') !!}   
-						{!! Form::close() !!}
+                                    <!-- Tab panes -->
+                                    <div class="tab-content">  
+                                    	<ul class="nav nav-tabs" role="tablist">
+                                        <li role="presentation" class="active"><a href="#au" aria-controls="home" role="tab" data-toggle="tab">Data User</a></li>
+                                        <li role="presentation"><a href="#tt" aria-controls="profile" role="tab" data-toggle="tab">Create user</a></li>
+                                        <li role="presentation"><a href="#vm" aria-controls="messages" role="tab" data-toggle="tab">Admin</a></li>
+                                        <li role="presentation"><a href="#tp" aria-controls="settings" role="tab" data-toggle="tab">Staff Admin</a></li>
+                                        <li role="presentation"><a href="#lu" aria-controls="settings" role="tab" data-toggle="tab">Volunteer</a></li>
+                                        <li role="presentation"> <a href="{{url('/editprofile')}}" >Edit Profile</a></li>
+                                    </ul>    
+                                    <hr>
+                                                                    
+                                        <div role="tabpanel" class="tab-pane active" id="au">
+                                        <div class="row">
+                                        <div class="col-md-10 col-md-offset-1">
+            <table class="table table-bordered">
+            	                        <h3>Data User</h3>
+                                        <thead>
+                                        <th>Username</th>
+                                        <th>Email</th>
+                                        <th>Level</th>
+                                        <th>Status</th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        </thead>
+                    <tbody>
+                    @foreach ($user as $data)
+                    <tr>
+                        <td>{{ $data->username }}</td>
+                        <td>{{ $data->email}}</td>
+                        @if($data->auth_level=="0")
+                        <td>Super Admin</td>
+                        @elseif($data->auth_level=="1")
+                        <td>Admin</td>
+                        @elseif($data->auth_level=="2")
+                        <td>Staff</td>
+                        @else
+                        <td>Volunteer</td>
+                        @endif
 
-						</td>
-						<td><a href="{{url('/cruduser/edit/'.$data->id)}}"><b>Edit Data<b></a>
-						<td><a href="{{url('/cruduser/resets/'.$data->id)}}"><b>Reset Password<b></a>  </td>  </td>
-					</tr>
-					@endforeach
-				</tbody>
-			</table>
-			<a href="{{url('/editprofile')}}">Kembali</a>
-		</div>
-	</div>
+                        @if($data->is_aktif=="0")   
+                        <td>Tidak aktif</td>
+                        @else
+                        <td>Aktif</td>
+                        @endif  
+                        <td>                
+                        {!! Form::open(['method' => 'DELETE', 'route' => ['user.destroy', $data->id]]) !!}
+                        {!! Form::submit('Hapus') !!}   
+                        {!! Form::close() !!}
+
+                        </td>
+                        <td><a href="{{url('/cruduser/edit/'.$data->id)}}"><b>Edit Data<b></a>
+                        <td><a href="{{url('/cruduser/resets/'.$data->id)}}"><b>Reset Password<b></a>  </td>  </td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+            </table>
+            
+        </div></div><hr></div>
+                                    
 
 
+
+
+
+        
+
+        <div role="tabpanel" class="tab-pane" id="vm">
+        <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <table class="table table-bordered">
+            	<h3>Admin</h3>
+                <thead>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Level</th>
+                    <th>Status</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach ($user as $data)
+                    @if($data->auth_level=="1")
+                    <tr>
+                        <td>{{ $data->username }}</td>
+                        <td>{{ $data->email}}</td>
+                        <td>Admin</td>
+                        @if($data->is_aktif=="0")   
+                        <td>Tidak aktif</td>
+                        @else
+                        <td>Aktif</td>
+                        @endif  
+                        <td>                
+                        {!! Form::open(['method' => 'DELETE', 'route' => ['user.destroy', $data->id]]) !!}
+                        {!! Form::submit('Hapus') !!}   
+                        {!! Form::close() !!}
+
+                        </td>
+                        <td><a href="{{url('/cruduser/edit/'.$data->id)}}"><b>Edit Data<b></a>
+                        <td><a href="{{url('/cruduser/resets/'.$data->id)}}"><b>Reset Password<b></a>  </td>  </td>
+                    </tr>
+                    @endif
+                    @endforeach
+                </tbody>
+            </table>
+          
+        </div>
+    </div>
+                                    <hr>    </div>
+
+
+
+
+
+
+
+                                        
+<div role="tabpanel" class="tab-pane" id="tt">
+<?php 
+use App\AppUtility;
+
+?>
+
+<div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <table class="table table-bordered">
+            <h3>Create User</h3>
+
+
+ {!! Form::model( new App\User, [
+    'route' => ['user.store']
+]) !!}
+
+<div class="form-g">
+    {!! Form::label('username', 'Username:') !!}
+    <br>{!! Form::text('username') !!}
 </div>
- 
-@stop
 
+<div class="form-g">
+    {!! Form::label('password', 'Password:') !!}
+    <br>{!! Form::password('password') !!}
+</div>
+
+<div class="form-g">
+    {!! Form::label('password_confirmation', 'Ulang Password:') !!}
+    <br>{!! Form::password('password_confirmation') !!}
+</div>
+
+<div class="form-g">
+    {!! Form::label('email', 'Email:') !!}
+   <br> {!! Form::text('email') !!}
+</div>
+<div class="form-g">
+    {!! Form::label('auth_level', 'Level:') !!} <br>
+    {!! Form::select('auth_level', array('0' => 'Super Admin', '1' => 'Admin',  '2' => 'Staff', '3' => 'Volunteer')); !!}
+</div>
+<div class="form-g">
+    {!! Form::label('is_aktif', 'Status:') !!}
+    <br>{!! Form::select('is_aktif', array('0' => 'Tidak Aktif', '1' => 'Aktif')); !!}
+</div>
+{!! Form::submit('Update', ['class' => 'btn btn-primary']) !!}
+{!! Form::close() !!}
+ </table>
+</div>
+</div><hr>
+</div>
+                                        
+        <div role="tabpanel" class="tab-pane" id="tp">                                           
+        <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <table class="table table-bordered">
+            	<h3>Staff Admin</h3> 
+                <thead>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Level</th>
+                    <th>Status</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach ($user as $data)
+                    @if($data->auth_level=="2")
+                    <tr>
+                        <td>{{ $data->username }}</td>
+                        <td>{{ $data->email}}</td>
+                        <td>Staff Admin</td>
+                        @if($data->is_aktif=="0")   
+                        <td>Tidak aktif</td>
+                        @else
+                        <td>Aktif</td>
+                        @endif  
+                        <td>                
+                        {!! Form::open(['method' => 'DELETE', 'route' => ['user.destroy', $data->id]]) !!}
+                        {!! Form::submit('Hapus') !!}   
+                        {!! Form::close() !!}
+
+                        </td>
+                        <td><a href="{{url('/cruduser/edit/'.$data->id)}}"><b>Edit Data<b></a>
+                        <td><a href="{{url('/cruduser/resets/'.$data->id)}}"><b>Reset Password<b></a>  </td>  </td>
+                    </tr>
+                    @endif
+                    @endforeach
+                </tbody>
+            </table>
+            
+        </div>
+    </div><hr>
+                                        </div>
+                                        
+                                        
+
+        <div role="tabpanel" class="tab-pane" id="lu">                                          
+        <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <table class="table table-bordered">
+            	<h3>Volunteer</h3> 
+                <thead>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Level</th>
+                    <th>Status</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach ($user as $data)
+                    @if($data->auth_level=="3")
+                    <tr>
+                        <td>{{ $data->username }}</td>
+                        <td>{{ $data->email}}</td>
+                        <td>Volunteer</td>
+                        @if($data->is_aktif=="0")   
+                        <td>Tidak aktif</td>
+                        @else
+                        <td>Aktif</td>
+                        @endif  
+                        <td>                
+                        {!! Form::open(['method' => 'DELETE', 'route' => ['user.destroy', $data->id]]) !!}
+                        {!! Form::submit('Hapus') !!}   
+                        {!! Form::close() !!}
+
+                        </td>
+                        <td><a href="{{url('/cruduser/edit/'.$data->id)}}"><b>Edit Data<b></a>
+                        <td><a href="{{url('/cruduser/resets/'.$data->id)}}"><b>Reset Password<b></a>  </td>  </td>
+                    </tr>
+                    @endif
+                    @endforeach
+                </tbody>
+            </table>
+           
+        </div>
+    </div><hr>
+                                        </div></div>
+                                    </div>
+                                </div>
+                             </div>
+                           
+                    </div><!--/.blog-item-->
+                   
+                </div><!--/.col-md-8-->
+                
+            </div>
+        </div>
+        
+    </section><!--/#blog-->
+@endsection
