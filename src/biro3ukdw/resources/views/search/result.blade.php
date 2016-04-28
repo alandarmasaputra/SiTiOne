e651be53bd4ab9f36be38977f9ad8e4f37e377bf
Christiana Wahyunita <christianawahyunita@gmail.com>
2016-04-28 13:48:33 +0700

    no message

--- src/biro3ukdw/resources/views/search/result.blade.php
+++ src/biro3ukdw/resources/views/search/result.blade.php
@@ -12,7 +12,9 @@ Event - Biro3 | UKW
     </div>
 	
     <div class="result-event">
-		<h1>Event</h1>
+		<div class="judul-event">
+			<h1>Event</h1>
+		</div>
 				@foreach($events as $event)
 				<a href="{{url('/event/'.$event->id)}}" style="display:block;">
 					<h2>{{ $event->name }}</h2><h2>{{ $event->kategori }} </h2> <h2> {{ $event->sumber }} </h2> <h2> {{ $event->tempat }}</h2>
@@ -22,7 +24,9 @@ Event - Biro3 | UKW
 	
 	<div></div>
     <div class="result-ukm">
-		<h1>UKM</h1>
+		<div class="judul-UKM">
+			<h1>UKM</h1>
+		</div>
 				@foreach($ukms as $ukm)
 				<a href="{{url('/ukm/'.$ukm->id)}}" style="display:block;">
 				<h2>{{ $ukm->name }}</h2>
@@ -32,7 +36,9 @@ Event - Biro3 | UKW
 	
 	
     <div class="result-news">
-		<h1>News</h1>
+		<div class="judul-news">
+			<h1>News</h1>
+		</div>
 				@foreach($news as $new)
 				<a href="{{url('/news/'.$new->id)}}" style="display:block;">
 				<h2>{{ $new->name }}</h2><h2> {{ $new->kategori }}</h2>
@@ -42,7 +48,9 @@ Event - Biro3 | UKW
 	
 	
     <div class="result-beasiswa">
-		<h1>Beasiswa</h1>
+		<div class="judul-beasiswa">
+			<h1>Beasiswa</h1>
+		</div>
 				@foreach($beasiswas as $beasiswa)
 				<a href="{{url('/beasiswa/'.$beasiswa->id)}}" style="display:block;">
 				<h2>{{ $beasiswa->name }}</h2><h2> {{ $beasiswa->kategori }} {{ $beasiswa->sumber }}</h2>
