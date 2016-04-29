4c400b17cff7e4672c40290313f65eece0a97d39
Jonathan Aditya <jonathan.aditya@ti.ukdw.ac.id>
2016-04-29 16:57:27 +0700

    result

--- src/biro3ukdw/resources/views/search/result.blade.php
+++ src/biro3ukdw/resources/views/search/result.blade.php
@@ -5,19 +5,23 @@ Event - Biro3 | UKW
 
 @section('body_content')
 <div class="container">
-	<div
     <div class="page-header">
         <h2>
             Search Result
         </h2>
     </div>
-	
 	<div class="row">
 		<div class="result-event">
+			<div class="judul-event">
 			<h1>Event</h1>
+		</div>
 					@foreach($events as $event)
-					<a href="{{url('/event/'.$event->id)}}" style="display:inline-block;">
-						<h2>Nama Event : {{ $event->name }}</h2><h2><?php
+					<div class="text-left">
+					<a href="{{url('/event/'.$event->id)}}">
+						Nama Event : {{ $event->name }}
+						</a>
+						</div>
+						<div class="text-left"><?php
 				$tags = $event->kategori;
 				$tags = explode(' ',$tags);
 				?>
@@ -29,34 +33,50 @@ Event - Biro3 | UKW
 					
 				</span>
 				@endif
-				@endforeach </h2> <h2> {{ $event->sumber }} </h2> <h2> {{ $event->tempat }}</h2>
-					</a>
+				@endforeach </div> 
+						<div class="text-left"> {{ $event->sumber }}</div> 
+						<div class="text-left"> {{ $event->tempat }}</div>
+
 					<br>
+					
 					@endforeach
 					@if (count($events) === 0)
-					Tidak ada yang sesuai dengan kata kunci...
+					Tidak ada yang sesuai dengan kata kunci
 					@endif
 		</div>
 
 		<div></div>
 		<div class="result-ukm">
+			<div class="judul-UKM">
 			<h1>UKM</h1>
+		</div>
 					@foreach($ukms as $ukm)
-					<a href="{{url('/ukm/'.$ukm->id)}}" style="display:inline-block;">
-					<h2>Nama UKM : {{ $ukm->name }}</h2>
-					</a>
+					<div class="text-left">
+					<a href="{{url('/ukm/'.$ukm->id)}}">
+						Nama UKM : {{ $ukm->name }}
+						</a>
+						</div>
+
+					<br>
+					
 					@endforeach
 					@if (count($ukms) === 0)
-					Tidak ada yang sesuai dengan kata kunci...
+					Tidak ada yang sesuai dengan kata kunci
 					@endif
 		</div>
 
 
 		<div class="result-news">
+			<div class="judul-news">
 			<h1>News</h1>
+		</div>
 					@foreach($news as $new)
-					<a href="{{url('/news/'.$new->id)}}" style="display:inline-block;">
-					<h2>Nama Berita : {{ $new->name }}</h2><h2><?php
+					<div class="text-left">
+					<a href="{{url('/news/'.$new->id)}}">
+						Nama Berita : {{ $new->name }}
+						</a>
+						</div>
+						<div class="text-left"><?php
 				$tags = $new->kategori;
 				$tags = explode(' ',$tags);
 				?>
@@ -68,20 +88,29 @@ Event - Biro3 | UKW
 					
 				</span>
 				@endif
-				@endforeach </h2> 
-					</a>
+				@endforeach </div> 
+					
+
+					<br>
+					
 					@endforeach
 					@if (count($news) === 0)
-					Tidak ada yang sesuai dengan kata kunci...
+					Tidak ada yang sesuai dengan kata kunci
 					@endif
 		</div>
 
 
 		<div class="result-beasiswa">
+			<div class="judul-beasiswa">
 			<h1>Beasiswa</h1>
+		</div>
 					@foreach($beasiswas as $beasiswa)
-					<a href="{{url('/beasiswa/'.$beasiswa->id)}}" style="display:inline-block;">
-					<h2>Nama Beasiswa : {{ $beasiswa->name }}</h2><h2><?php
+					<div class="text-left">
+					<a href="{{url('/beasiswa/'.$beasiswa->id)}}">
+						Nama Beasiswa : {{ $beasiswa->name }}
+						</a>
+						</div>
+						<div class="text-left"><?php
 				$tags = $beasiswa->kategori;
 				$tags = explode(' ',$tags);
 				?>
@@ -93,15 +122,18 @@ Event - Biro3 | UKW
 					
 				</span>
 				@endif
-				@endforeach </h2> <h2> {{ $beasiswa->sumber }}</h2>
-					</a>
+				@endforeach </div> 
+						<div class="text-left"> {{ $beasiswa->sumber }}</div> 
+						
+
+					<br>
+					
 					@endforeach
 					@if (count($beasiswas) === 0)
-					Tidak ada yang sesuai dengan kata kunci...
+					Tidak ada yang sesuai dengan kata kunci
 					@endif
+
 		</div>
 	</div>
-	
-	
 </div>
 @endsection
