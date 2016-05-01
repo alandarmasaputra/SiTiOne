b2818bb8c6efd48a3e611b64959efee991a919e0
Alan Darmasaputra <alan.darmasaputra@ti.ukdw.ac.id>
2016-05-01 18:16:56 +0700

    no message

--- src/biro3ukdw/resources/views/errors/500.blade.php
+++ src/biro3ukdw/resources/views/errors/500.blade.php
@@ -1,13 +1,11 @@
-@section('head_title')
-500
-@endsection
-
-
-@extends('layout.app')
-@section('body_content')
-<section class="container">
-	<div class="row">
-		<div class="card text-center">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('style/images/ico/logo-title.png') }}">
+	<title>404</title>
+</head>
+<section style="width:100%;text-align:center;margin-top:10%;">
+	<div>
+		<div>
 			<h1>500</h1>
 			<br>
 			<p>Ada kesalahan pada server</p>
@@ -15,5 +13,4 @@
 			<a href="{{ url('/home') }}">Klik di sini</a> untuk kembali ke halaman beranda</p>
 		</div>
 	</div>
-</section><!--/#blog-->
-@endsection
+</section>
